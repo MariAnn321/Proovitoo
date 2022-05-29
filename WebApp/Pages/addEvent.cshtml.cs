e86f82926673702eb7906bf7419c26d55aae8188
Mari-Ann <mari-ann.piht@ttu.ee>
2022-05-29 13:05:17 +0300

    1

---
+++ WebApp/Pages/addEvent.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class AddEventCshtml : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public AddEventCshtml(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
